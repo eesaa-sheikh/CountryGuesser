b3bbd3f42cd968baa5f20caaeeb9a64781ddcc5d
eesaasheikh <eesaasheikh3@gmail.com>
2023-01-18 19:42:52 +0000

    improving the texContent

--- app.js
+++ app.js
@@ -15,29 +15,14 @@ const getCountryByAll = async () =>{
          const toDoList = document.querySelector("#list");
         
         
-         listCountry.textContent = "Name: " + country.name.common + " : " +" Population: " + country.population +
-         "Flag: " + country.flag;
+         listCountry.textContent = "Name= " + country.name.common + " : " +" Population= " + country.population + " : "+
+         "Flag = " + country.flag + ":" + "region" + country.region;
     
          toDoList.appendChild(listCountry);
+         
 })
 }
 
-
-// const getCountryByAll = async () =>{
-
-//     console.log("Refresh")
-
-//     const response = await fetch("https://restcountries.com/v3.1/all");
-//     const jsonData = await response.json();
-//     const conutrySection = document.querySelector("#countrySearch");
-//     jsonData.map((country)=> 
-//      {
-//         const newThing = document.createElement("p");
-//         newThing.textContent = "Name: " + country.name.comon
-//      })
-//   //  console.log(jsonData);
-
-// }
 
 
 const getCountryByName = async () =>{
@@ -54,7 +39,7 @@ const getCountryByName = async () =>{
 
     jsonData.map((country) =>{
 
-    const listCountry = document.createElement("ul");
+    const listCountry = document.createElement("ol");
 
      const toDoList = document.querySelector("#list");
     
@@ -66,28 +51,7 @@ const getCountryByName = async () =>{
 
 
     })
-    //display data
 
-    //create ul and li data
-
-    //§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
-  
-    // listCountry.textContent = JSON.stringify(jsonData, null, " ");
-
-    // console.log(listCountry)
-
-   // §§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§§
-
-
-
-
-//    console.log(listCountry.flag)
-
-
-
-//     toDoList.appendChild(listCountry);
-//    // console.log ("Also with cute Corgi")
-//     document.querySelector("li").appendChild(inputField.value);
 
 
 
@@ -101,38 +65,3 @@ const meme = document.querySelector("#newButton");
 
 
 meme.addEventListener("click",getCountryByAll);
-
-// creating an input  field
-// create a button
-
-
-
-
-
-
-
-
-
-// function getCountries() {
-//     const [countries, setCountries] = useState([]);
-   
-//     function handleCountryChange(countries) {
-//      setCountries(countries);
-//     }
-   
-//     useEffect(
-//      debounce(async () => {
-//       const data = await fetch(
-//        "https://restcountries.eu/rest/v2/all?fields=name"
-//       );
-//       const countries = await data.json();
-//       handleCountryChange(countries);
-//      }, 2000)
-//     );
-   
-//     return countries;
-//    }
-
-//    const button = document.querySelector("button");
-
-// button.addEventListener("click", getCountries);
